e59d6bcd978be05eac5cf8fdc49ad6cdea7b07fe
Zac <tclu82@uw.edu>
2017-02-08 00:34:44 -0800

    implement UIPickerView to pick state

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,20 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
 
+    @IBOutlet private weak var statePicker: UIPickerView!
+    
+    @IBOutlet private weak var statePickerButton: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York", "Washington"]
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        statePicker.dataSource = self
         
+        statePicker.delegate = self
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        statePickerButton.setTitle(states[row], for: UIControlState.normal)
+//        statePickerButton.setTitle(states[row], for: UIControlState()) // workaround
+        
+        statePicker.isHidden = true
+    }
+    
+    @IBAction private func stateButtonPress(_ sender: UIButton)
+    {
+        statePicker.isHidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
